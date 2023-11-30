aacd65c46d412759fc3597ddae9964e297f836d2
Furkan Yıldız <40992809+Fy1717@users.noreply.github.com>
2023-11-30 15:05:27 +0300

    new aar version added

--- app/src/main/java/com/papilon/papilonfpdapp/MainActivity.java
+++ app/src/main/java/com/papilon/papilonfpdapp/MainActivity.java
@@ -2,14 +2,13 @@ package com.papilon.papilonfpdapp;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.papilon.papilonfinger.view.pap_DetectorActivity;
 import com.papilon.papilonfinger.model.pap_FingerPrintImages;
 
@@ -46,11 +45,15 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("from_main", "true");
 
         // license parameters
-        intent.putExtra("token_for_license", "");
-        intent.putExtra("license_id", "");
+        intent.putExtra("token_for_license", "4ad37eebab1462c38867b73e2a4cf7d48144bf176c55716f3abc59e9ce8c61bde8160ab9a8580b54f52daa9b05f4b50d1cae");
+        intent.putExtra("license_id", "9");
         // ----------------------------------------------------------------
 
-        startActivity(intent);
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.e("StartLibrary", "Error : " + e.getMessage());
+        }
     }
 
     @Override
